fd576754a4664a3961d0e017500374d6b7418bb9
joma74 <dev-mails@gmx.net>
2018-09-05 15:31:19 +0200

    chore(typing): How to type a Parameter as Object

--- test/helpers/mock-fs.js
+++ test/helpers/mock-fs.js
@@ -1,6 +1,21 @@
 'use strict';
+
+/**
+ * @typedef {import ("../../types").KeysAndAnyValues<string, any>} KeysAndAnyValues
+ */
+
 module.exports = {
-    files: {},
+	/**
+	 * @type {KeysAndAnyValues}
+	 */
+	files: {},
+	/**
+	 * @todo Write TS issue for typeof Object.prototype
+	 * @see [T.constructor should be of type T|https://github.com/Microsoft/TypeScript/issues/3841]
+	 * 
+	 * @param {string} path 
+	 * @param {typeof Object.prototype} content 
+	 */
     outputFileSync: function(path, content) {
         this.files[path] = content.toString();
     },
