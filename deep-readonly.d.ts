5e554ddbbbe78acab4d66546ad4aef06616ff59e
joma74 <dev-mails@gmx.net>
2018-09-05 15:30:24 +0200

    chore(typing): A DeepReadonly type definition

---
+++ deep-readonly.d.ts
@@ -0,0 +1,12 @@
+type DeepReadonly<T> =
+    T extends any[] ? DeepReadonlyArray<T[number]> :
+    T extends object ? DeepReadonlyObject<T> :
+	T;
+	
+interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+
+type NonFunctionPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? never : K }[keyof T];
+
+type DeepReadonlyObject<T> = {
+    readonly [P in NonFunctionPropertyNames<T>]: DeepReadonly<T[P]>;
+};
